5cf928e116e8e1e09e265b923254277f649cd209
Gian Zignago <55062485+zignago@users.noreply.github.com>
2020-09-22 23:40:54 -0500

    Add files via upload

---
+++ Kattis/C++/r2.cpp
@@ -0,0 +1,15 @@
+//open.kattis.com/problems/r2
+//CPU: 0.00s
+
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    int num, mean;
+    cin >> num >> mean;
+    
+    cout << (mean * 2) - num;
+    
+    return 0;
+}
